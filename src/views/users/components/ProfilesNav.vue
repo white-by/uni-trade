d2c9465a1bc54aedbd7542fde42a975be8b5bbd9
whiteby <2115182149@qq.com>
2024-11-05 16:38:27 +0800

    fix:profiles页nav导航栏的切换bug

--- src/views/users/components/ProfilesNav.vue
+++ src/views/users/components/ProfilesNav.vue
@@ -1,17 +1,51 @@
-<script setup></script>
+<script setup>
+import { ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+const activeIndex = ref('')
+
+// 根据当前路径设置激活的导航项
+watch(
+  () => route.path,
+  (newPath) => {
+    if (newPath === '/profiles/published' || newPath === '/profiles') {
+      activeIndex.value = '1'
+    } else if (newPath === '/profiles/finished') {
+      activeIndex.value = '2'
+    } else if (newPath === '/profiles/comment') {
+      activeIndex.value = '3'
+    }
+  },
+  { immediate: true }
+)
+
+// 切换导航项
+function menuSelect(index) {
+  activeIndex.value = index
+  if (index === '1') {
+    router.push('/profiles/published')
+  } else if (index === '2') {
+    router.push('/profiles/finished')
+  } else if (index === '3') {
+    router.push('/profiles/comment')
+  }
+}
+</script>
 
 <template>
   <div class="container">
     <el-menu :default-active="activeIndex" mode="horizontal" @select="menuSelect">
-      <el-menu-item index="1" :class="{ active: $route.path === '/profiles/published' || $route.path === '/profiles' }">
-        <router-link to="/profiles/published"> 已发布</router-link></el-menu-item
-      >
-      <el-menu-item index="2" :class="{ active: $route.path === '/profiles/finished' }"
-        ><router-link to="/profiles/finished">已完成</router-link></el-menu-item
-      >
-      <el-menu-item index="3" :class="{ active: $route.path === '/profiles/comment' }">
-        <router-link to="/profiles/comment">交易评价</router-link></el-menu-item
-      >
+      <el-menu-item index="1">
+        <router-link to="/profiles/published">已发布</router-link>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <router-link to="/profiles/finished">已完成</router-link>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <router-link to="/profiles/comment">交易评价</router-link>
+      </el-menu-item>
     </el-menu>
   </div>
 </template>
@@ -21,7 +55,7 @@
   width: auto;
 }
 
-.active {
+.el-menu-item.is-active {
   background-color: white; /* 选中时的背景颜色 */
   color: $comColor; /* 选中时的文字颜色 */
   border-color: $comColor;

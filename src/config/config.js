1adc2c8f72ad07cf470fed8cadf9320096dc811f
whiteby <2115182149@qq.com>
2024-12-11 12:16:14 +0800

    none

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@
 // config.js
 export const config = {
   //apifox云端MOCK
-  baseURL: 'https://apifoxmock.com/m1/5227100-4893915-default',
+  // baseURL: 'https://apifoxmock.com/m1/5227100-4893915-default',
   //apifox本地MOCK
   // baseURL: 'http://127.0.0.1:4523/m1/5227100-4893915-default',
   baseURL: 'http://127.0.0.1:5001',

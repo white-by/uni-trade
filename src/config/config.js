95c417004af9d64016dbbd39567f9867f8741fec
whiteby <2115182149@qq.com>
2024-12-11 10:41:08 +0800

    feat:收藏/取消收藏api

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ export const config = {
   //apifox云端MOCK
   // baseURL: 'https://apifoxmock.com/m1/5227100-4893915-default',
   //apifox本地MOCK
-  // baseURL: 'http://127.0.0.1:4523/m1/5227100-4893915-default',
-  baseURL: 'http://127.0.0.1:5001',
+  baseURL: 'http://127.0.0.1:4523/m1/5227100-4893915-default',
+  // baseURL: 'http://127.0.0.1:5001',
   timeout: 5000
 }

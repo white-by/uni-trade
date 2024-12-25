0373842db6109bf1789258b1dd305562faabc6f3
boreascup <1812750453@qq.com>
2024-12-25 19:36:24 +0800

    数据库更新

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,9 @@ export const config = {
   // baseURL: 'https://apifoxmock.com/m1/5227100-4893915-default',
   //apifox本地MOCK
   // baseURL: 'http://127.0.0.1:4523/m1/5227100-4893915-default',
+  //公网转发后端
   // baseURL: 'https://mpjqk7qz-5001.asse.devtunnels.ms/',
+  //本地后端
   baseURL: 'http://127.0.0.1:5001',
   timeout: 5000
 }
